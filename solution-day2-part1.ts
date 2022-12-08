19d4c04a8c82e5adfbbecbebe1e1f37dfb0e66da
James Pike <jpike@chilon.net>
2022-12-09 00:34:36 +0800

    solution day 2 part 1

---
+++ solution-day2-part1.ts
@@ -0,0 +1,44 @@
+#!/usr/bin/env -S npx ts-node
+
+import { readFile } from "fs/promises"
+
+const oppenentCodes = new Map<string, number>([
+  ["A", 0],
+  ["B", 1],
+  ["C", 2],
+])
+const myCodes = new Map<string, number>([
+  ["X", 0],
+  ["Y", 1],
+  ["Z", 2],
+])
+
+const drawScore = 3
+const winScore = 6
+
+const score = (opponentHand: number, myHand: number) => {
+  const handScore = myHand + 1
+  if (opponentHand === myHand) {
+    return drawScore + handScore
+  } else if (myHand === (opponentHand + 1) % 3) {
+    return winScore + handScore
+  } else {
+    return handScore
+  }
+}
+
+;(async () => {
+  const lines = (await readFile("input-2.txt")).toString().trim().split("\n")
+  console.log(
+    lines
+      .map((line) => {
+        const opponentHand = oppenentCodes.get(line[0])
+        const myHand = myCodes.get(line[2])
+        if (opponentHand === undefined) throw new Error(`Bad opponent code ${line[0]}`)
+        if (myHand === undefined) throw new Error(`Bad code for me ${line[1]}`)
+
+        return score(opponentHand, myHand)
+      })
+      .reduce((a, b) => a + b)
+  )
+})()
